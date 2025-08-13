7b1b32bffbf01992b84fa1372c2fde6c00c701a8
Shishir Rijal <ccrrizal0892@gmail.com>
2025-08-13 19:58:46 +0545

    Add custom colors

---
+++ CryptoPortfolio/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  CryptoPortfolio
+//
+//  Created by Shishir Rijal on 13/08/2025.
+//
+
+
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    let blue = Color("Blue")
+    let lightBlue = Color("LightBlue")
+    let gray = Color("Gray")
+}
---
+++ CryptoPortfolio/Views/Analytics/Components/AppTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  AppTabBar.swift
+//  CryptoPortfolio
+//
+//  Created by Shishir Rijal on 13/08/2025.
+//
+
+import SwiftUI
+
+struct AppTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppTabBar()
+}
