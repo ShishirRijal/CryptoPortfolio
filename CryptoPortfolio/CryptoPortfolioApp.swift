28ddbe842a7ea5b2b424c53176ad96f934b93ce4
Shishir Rijal <ccrrizal0892@gmail.com>
2025-08-13 18:53:48 +0545

    Initial Commit

---
+++ CryptoPortfolio/CryptoPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioApp.swift
+//  CryptoPortfolio
+//
+//  Created by Shishir Rijal on 13/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
