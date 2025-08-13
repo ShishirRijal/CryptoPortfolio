c130a513a5a05c63f398b54761159295cf8e4073
Shishir Rijal <ccrrizal0892@gmail.com>
2025-08-13 22:43:12 +0545

    create transaction row view

--- CryptoPortfolio/Views/Exchange/Components/TransactionRowView.swift
+++ CryptoPortfolio/Views/Exchange/Components/TransactionRowView.swift
@@ -1,3 +1,11 @@
+//
+//  TransactionRowView.swift
+//  CryptoPortfolio
+//
+//  Created by Shishir Rijal on 13/08/2025.
+//
+
+import SwiftUI
 
 struct TransactionRowView: View {
     let transaction: Transaction
